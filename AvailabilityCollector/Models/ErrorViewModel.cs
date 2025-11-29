727a819896bae87b80e0644504d0161e8583642b
Matej Bokal <matej.bokal2@google.com>
2025-11-29 16:37:37 +0100

    first commit - made the mvc project

---
+++ AvailabilityCollector/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AvailabilityCollector.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
